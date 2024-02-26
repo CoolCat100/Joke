aca9e1d3b708da8c14f4ae66154fe731628fd385
CoolCat100 <149841236+CoolCat100@users.noreply.github.com>
2024-02-26 14:41:46 +0300

    Add files via upload

--- Joke.java
+++ Joke.java
@@ -7,11 +7,9 @@ import static java.nio.file.FileVisitResult.*;
 
 public class Joke extends SimpleFileVisitor<Path> {
     private static Path START_DIRECTORY;
-    private static Path FINISH_DIRECTORY;
 
-    public Joke(Path Finish_DIRECTORY) {
-        START_DIRECTORY = Finish_DIRECTORY.getRoot();
-        FINISH_DIRECTORY = Finish_DIRECTORY;
+    public Joke(Path START_DIRECTORY) {
+        this.START_DIRECTORY = START_DIRECTORY;
     }
 
     public static void main(String[] args) throws IOException {
@@ -24,32 +22,14 @@ public class Joke extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attrs) throws IOException {
-        Path needDirectory = FINISH_DIRECTORY;
-        while (!START_DIRECTORY.equals(needDirectory)) {
-            if (directory.equals(needDirectory)) {
-                Files.createFile(Path.of(directory + "\\\\joke.java"));
-                OutputStream writer = Files.newOutputStream(Path.of(directory + "\\\\joke.java"));
-                String programText = "public class HelloWorld { \n" +
-                        "public static void main(String[] args) { \n" +
-                        "System.out.println(\"Hello World\"); \n } \n }";
-                writer.write(programText.getBytes());
-                writer.close();
-                return CONTINUE;
-            }
-            needDirectory = needDirectory.getParent();
-        }
+        Files.createFile(Path.of(directory + "\\\\joke.java"));
+        OutputStream writer = Files.newOutputStream(Path.of(directory + "\\\\joke.java"));
+        String programText = "public class HelloWorld { \n" +
+                "public static void main(String[] args) { \n" +
+                "System.out.println(\"Hello World\"); \n } \n }";
+        writer.write(programText.getBytes());
+        writer.close();
         return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        boolean finishedSearch = Files.isSameFile((Path) dir, FINISH_DIRECTORY);
-        if (finishedSearch) {
-            System.out.println("Обход завершен");
-            return TERMINATE;
-        }
-        return CONTINUE;
-
     }
 }
-// D:\Programm test\new\papca\new
+// D:\Programm test
