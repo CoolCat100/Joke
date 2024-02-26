3951f5089eabaf895536778c59aca0fe6b710caa
CoolCat100 <149841236+CoolCat100@users.noreply.github.com>
2024-02-26 14:57:53 +0300

    Add files via upload

--- Joke.java
+++ Joke.java
@@ -1,35 +1,17 @@
+package Joke;
+
 import java.io.*;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Scanner;
 
-import static java.nio.file.FileVisitResult.*;
 
-public class Joke extends SimpleFileVisitor<Path> {
-    private static Path START_DIRECTORY;
-
-    public Joke(Path START_DIRECTORY) {
-        this.START_DIRECTORY = START_DIRECTORY;
-    }
-
+public class Joke {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
         Path startingDir = Paths.get(path);
-        Joke joke = new Joke(startingDir);
-        Files.walkFileTree(START_DIRECTORY, joke);
-    }
-
-    @Override
-    public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attrs) throws IOException {
-        Files.createFile(Path.of(directory + "\\\\joke.java"));
-        OutputStream writer = Files.newOutputStream(Path.of(directory + "\\\\joke.java"));
-        String programText = "public class HelloWorld { \n" +
-                "public static void main(String[] args) { \n" +
-                "System.out.println(\"Hello World\"); \n } \n }";
-        writer.write(programText.getBytes());
-        writer.close();
-        return CONTINUE;
+        JokeFileVisitor fileVisitor = new JokeFileVisitor(startingDir);
+        Files.walkFileTree(startingDir, fileVisitor);
     }
 }
 // D:\Programm test
---
+++ JokeFileVisitor.java
@@ -0,0 +1,35 @@
+package Joke;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class JokeFileVisitor extends SimpleFileVisitor<Path> {
+    private static Path START_DIRECTORY;
+    public static final String PROGRAM_TEXT = "public class HelloWorld { \n" +
+            "public static void main(String[] args) { \n" +
+            "System.out.println(\"Hello World\"); \n } \n }";
+
+    public JokeFileVisitor(Path START_DIRECTORY) {
+        this.START_DIRECTORY = START_DIRECTORY;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attrs) throws IOException {
+        Files.createFile(Path.of(directory + "\\\\joke.java"));
+        writeCod(directory);
+        return CONTINUE;
+    }
+
+    public void writeCod(Path directory) throws IOException {
+        OutputStream writer = Files.newOutputStream(Path.of(directory + "\\\\joke.java"));
+        writer.write(PROGRAM_TEXT.getBytes());
+        writer.close();
+    }
+}
